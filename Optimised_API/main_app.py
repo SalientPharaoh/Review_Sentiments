c492fd449ecdfa677a40e407a95a0f48aa72fd70
Sahil Khirwal <sahilkhirwal1012@gmail.com>
2024-09-27 02:54:02 +0530

    adding minor changes

--- Optimised_API/main_app.py
+++ Optimised_API/main_app.py
@@ -24,7 +24,7 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI(title="Optimized Sentiment Analysis API")
 groq_client = AsyncGroq(api_key=os.getenv("GROQ_API_KEY"))
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("./static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
